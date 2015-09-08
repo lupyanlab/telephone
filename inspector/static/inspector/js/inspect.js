c950c02add64a466ad31e0d7b55696e2446140c7
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-09-08 11:52:19 -0500

    Add semicircles as brush handles

--- inspector/static/inspector/js/inspect.js
+++ inspector/static/inspector/js/inspect.js
@@ -204,13 +204,16 @@ function createPlaybar(message) {
 
   var timeScale = d3.scale.linear();
 
-  var now = Date.now();
-
   var messageDuration = soundManager.sounds[message.soundId].duration;
 
   timeScale
     .domain([0, messageDuration])
     .range([0, playerWidth]);
+
+  var timeScrub = d3.svg.brush();
+
+  timeScrub
+    .x(timeScale)
 
   var timeAxis = d3.svg.axis();
 
@@ -224,5 +227,21 @@ function createPlaybar(message) {
     .attr("class", "time axis")
     .attr("transform", "translate(0,10)")
     .call(timeAxis);
+
+  var arc = d3.svg.arc()
+    .outerRadius(playerHeight / 2)
+    .startAngle(0)
+    .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
+
+  var brushg = svgFocus.append("g")
+    .attr("class", "time brush")
+    .call(timeScrub);
+
+  brushg.selectAll(".resize").append("path")
+    .attr("transform", "translate(0," +  playerHeight / 2 + ")")
+    .attr("d", arc);
+
+  brushg.selectAll("rect")
+      .attr("height", playerHeight);
 
 }
