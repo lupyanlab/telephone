2b688fad3ff21f9659f7091dbd9ab40bb8349f7a
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-09-10 08:50:12 -0500

    Move validation and cleaning to custom field

--- ratings/forms.py
+++ ratings/forms.py
@@ -4,28 +4,25 @@ from django.core.exceptions import ValidationError
 from grunt.models import Message
 
 
-class SurveyForm(forms.Form):
-    # message pks, comma-separated
-    # A rating will be created for each message
-    questions = forms.CharField()
-
-    # message pks, comma-separated
-    # All choices are present for every rating
-    choices = forms.CharField()
-
-    def clean_questions(self):
-        self.verify_message_str(self.cleaned_data.get('questions'))
-
-    def clean_choices(self):
-        self.verify_message_str(self.cleaned_data.get('choices'))
-
-    def verify_message_str(self, message_str):
+class MessageIdField(forms.Field):
+    def to_python(self, value):
         try:
-            message_ids = map(int, message_str.split(','))
+            return map(int, value.split(','))
         except ValueError:
             raise ValidationError('Messages must be given as ints')
 
+    def validate(self, value):
         all_message_ids = Message.objects.values_list('id', flat=True)
-        for message_id in message_ids:
+        for message_id in value:
             if message_id not in all_message_ids:
                 raise ValidationError('Message not found')
+
+
+class SurveyForm(forms.Form):
+    # message pks, comma-separated
+    # A rating will be created for each message
+    questions = MessageIdField()
+
+    # message pks, comma-separated
+    # All choices are present for every rating
+    choices = MessageIdField()
