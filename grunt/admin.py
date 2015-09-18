d7de7aa0b50f514bbcf65b9a8552038334ee55c4
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-09-17 22:28:52 -0500

    Add actions to admin

--- grunt/admin.py
+++ grunt/admin.py
@@ -2,10 +2,26 @@ from django.contrib import admin
 
 from .models import Game, Chain, Message
 
-@admin.register(Game, Chain)
+
+def kill_message(modeladmin, request, queryset):
+    """ Set the selected messages num_children to zero. """
+    queryset.update(num_children=0)
+kill_message.short_description = "Prevent selected messages from replicating"
+
+
+@admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name')
+
+
+@admin.register(Chain)
+class ChainAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'game')
+    list_filter = ('game', )
+
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'chain', 'generation', 'parent', 'audio')
+    list_display = ('id', 'chain', 'generation', 'parent', 'audio', 'num_children')
+    actions = [kill_message, ]
+    list_filter = ('chain', )
