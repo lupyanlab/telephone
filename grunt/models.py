491bc4de60166b1b34ab8327a608448d9e011585
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-09-21 14:07:41 -0500

    Commented models

--- grunt/models.py
+++ grunt/models.py
@@ -12,6 +12,10 @@ class Game(models.Model):
     name = models.CharField(max_length=30)
 
     def pick_next_message(self, receipts=[]):
+        """ Determine which message the player should receive next.
+
+        TODO: move this function to handlers
+        """
         families = Message.objects.filter(id__in=receipts).values_list(
             'chain', flat=True
         )
@@ -28,6 +32,10 @@ class Chain(models.Model):
     name = models.CharField(max_length=30)
 
     def pick_parent(self):
+        """ Select a fertile message at random.
+
+        TODO: move this function to handlers
+        """
         return self.messages.filter(num_children__gt=0).order_by('?')[0]
 
     def __str__(self):
@@ -49,6 +57,10 @@ class Message(models.Model):
             self.save()
 
     def find_ancestor(self, possible_ancestors):
+        """ Trace lineage trying to find someone in possible_ancestors.
+
+        TODO: move this function to handlers
+        """
         if not self.parent:
             raise Message.DoesNotExist('No ancestors found in choices')
         elif self.parent in possible_ancestors:
@@ -57,6 +69,10 @@ class Message(models.Model):
             return self.parent.find_ancestor(possible_ancestors)
 
     def __str__(self):
+        """ An uninterpretable hash of this message.
+
+        str(message) is used to label the choices in surveys!
+        """
         return hashlib.sha224('{}-{}-{}'.format(
             self.chain.game.id, self.chain.id, self.id
         )).hexdigest()[:10]
