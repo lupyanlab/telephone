25a3a45420c8fdf29e3b35a65b825eb0bc10c517
Igor Korsunov <elquendi@yandex.ru>
2015-11-03 01:39:27 +0300

    Update Backbone app to use new view for game tree

--- inspector/static/inspector/js/app.js
+++ inspector/static/inspector/js/app.js
@@ -1,9 +1,29 @@
-class App {
+import {GameTreeView} from 'inspector/tree.js';
+import {GameTree} from 'inspector/tree.js';
 
-  constructor() {
-    console.log("Hello, world!");
+class Router extends Backbone.Router {
+
+  get routes() {
+    return {
+      ':id/inspect(/)': "inspectGame"
+    }
+  }
+
+  inspectGame(gameId) {
+    let game = new GameTree({id: gameId});
+    this.view = new GameTreeView({model: game, el: $("svg.tree")});
+    game.fetch();
   }
 
 }
 
-new App();
+class App {
+
+  constructor() {
+    this.router = new Router();
+    Backbone.history.start({pushState: true});
+  }
+
+}
+
+const app = new App();
