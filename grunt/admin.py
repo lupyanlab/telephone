4de4a1a32aee0370c0923db5a78103aaf5fb9cf3
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-03-07 08:53:23 -0600

    Update model admin for batch applying rejects and approvals

--- grunt/admin.py
+++ grunt/admin.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from .models import Game, Chain, Message
 
 
-def kill_message(modeladmin, request, queryset):
-    """ Set the selected messages num_children to zero. """
-    queryset.update(num_children=0)
-kill_message.short_description = "Prevent selected messages from replicating"
+def reject_message(modeladmin, request, queryset):
+    """Batch reject messages."""
+    queryset.update(rejected=True)
+reject_message.short_description = "Prevent selected messages from replicating"
 
+def verify_message(modeladmin, request, queryset):
+    """Batch verify messages."""
+    queryset.update(verified=True)
+verify_message.short_description = "Mark selected messages as verified"
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
@@ -22,6 +26,6 @@ class ChainAdmin(admin.ModelAdmin):
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'chain', 'generation', 'parent', 'audio', 'num_children')
-    actions = [kill_message, ]
-    list_filter = ('chain', )
+    list_display = ('id', 'chain', 'generation', 'parent', 'audio', 'num_children', 'rejected', 'verified')
+    list_filter = ('chain', 'rejected', 'verified')
+    actions = [reject_message, verify_message]
