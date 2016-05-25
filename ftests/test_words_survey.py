1c78cbae278cdd75a0c5ac2f1bb4ddd2fbf9f960
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-05-25 11:43:51 -0500

    Refactor of words survey ftests

--- ftests/test_words_survey.py
+++ ftests/test_words_survey.py
@@ -1,3 +1,4 @@
+import unipath
 from model_mommy import mommy
 
 from words.forms import NewWordSurveyForm
@@ -7,24 +8,55 @@ from ftests.base import FunctionalTest
 
 class WordsTest(FunctionalTest):
     def setUp(self):
-        """Populate the DB with some messages to guess the transcriptions of."""
         super(WordsTest, self).setUp()
+        # Create the messages to use as choices
         mommy.make_recipe('grunt.seed', _quantity=4)
+        self.choices = Message.objects.values_list('id', flat=True)
+
+        # Use the same words in string and file form
+        self.words = ['booba', 'kiki']
+        self.txt_path = unipath.Path('test-words-upload.txt')
+        with open(self.txt_path, 'w') as f:
+            for w in self.words:
+                f.write(w)
+
+    def tearDown(self):
+        super(WordsTest, self).tearDown()
+        # Remove the words file
+        if self.txt_path.exists():
+            self.txt_path.remove()
+
+    def nav_to_word_surveys(self):
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_words_list').click()
 
 
-    def test_create_words_survey_from_message_ids(self):
-        # Get data required for new word survey form
-        choices = Message.objects.values_list('id', flat=True)
-        words = ['booba', 'kiki']
-
+class CreateWordsTest(WordsTest):
+    def setUp(self):
+        super(CreateWordsTest, self).setUp()
         # Navigate to new word survey form
         self.nav_to_word_surveys()
         self.browser.find_element_by_id('id_new_words').click()
 
+    def fill_form(self, element_id, text):
+        self.browser.find_element_by_id(element_id).send_keys(text)
+
+    def test_create_words_survey_from_words_string(self):
         # Fill out new word survey form
         self.fill_form('id_name', 'test new words')
-        self.fill_form('id_choices', stringify(choices))
-        self.fill_form('id_words', stringify(words))
+        self.fill_form('id_choices', stringify(self.choices))
+        self.fill_form('id_words', stringify(self.words))
+        self.browser.find_element_by_id('submit-id-submit').click()
+
+        # Land back at the word survey list page
+        page_title = self.browser.find_element_by_tag_name('h1').text
+        self.assertEquals(page_title, 'Word Surveys')
+
+    def test_create_words_survey_from_uploaded_csv(self):
+        # Fill out new word survey form
+        self.fill_form('id_name', 'test new words')
+        self.fill_form('id_choices', stringify(self.choices))
+        self.fill_form('id_words_file', self.txt_path)
         self.browser.find_element_by_id('submit-id-submit').click()
 
         # Land back at the word survey list page
@@ -32,18 +64,26 @@ class WordsTest(FunctionalTest):
         self.assertEquals(page_title, 'Word Surveys')
 
 
-    def test_take_words_survey(self):
-        # Create a word survey
-        choices = Message.objects.values_list('id', flat=True)
-        words = ['booba', 'kiki']
-        data = dict(
+class TakeSurvey(WordsTest):
+    def create_word_survey(self, **kwargs):
+        form_data = dict(
             name='ftest',
             num_questions_per_player=2,
-            words=stringify(words),
-            choices=stringify(choices),
+            words=stringify(self.words),
+            choices=stringify(self.choices),
         )
-        form = NewWordSurveyForm(data)
+        form_data.update(kwargs)
+        form = NewWordSurveyForm(form_data)
         form.save()
+
+    def submit_answer(self):
+        choices_css = "[type='radio']"
+        choices = self.browser.find_elements_by_css_selector(choices_css)
+        choices[1].click()
+        self.browser.find_element_by_id('submit-id-submit').click()
+
+    def test_take_words_survey(self):
+        self.create_word_survey()
 
         # Navigate to take survey
         self.nav_to_word_surveys()
@@ -51,15 +91,15 @@ class WordsTest(FunctionalTest):
 
         # See the first question
         given = self.browser.find_element_by_id('id_word').text
-        self.assertIn(given, words)
-        words.remove(given)
+        self.assertIn(given, self.words)
+        self.words.remove(given)
 
         # Select a choice
         self.submit_answer()
 
         # See the second question and answer it
         given = self.browser.find_element_by_id('id_word').text
-        self.assertIn(given, words)
+        self.assertIn(given, self.words)
         self.submit_answer()
 
         # Get completion code
@@ -68,20 +108,10 @@ class WordsTest(FunctionalTest):
         self.assertEquals(len(parts), 2)
 
 
-    def test_words_survey_with_catch_trial(self):
-        # Create a word survey with a catch trial
-        choices = Message.objects.values_list('id', flat=True)
-        words = ['booba', 'kiki']
+    def test_take_words_survey_with_catch_trial(self):
         catch_trial = 'this is the catch trial'
-        data = dict(
-            name='ftest',
-            num_questions_per_player=1,
-            words=stringify(words),
-            choices=stringify(choices),
-            catch_trial='this is the catch trial'
-        )
-        form = NewWordSurveyForm(data)
-        form.save()
+        self.create_word_survey(catch_trial=catch_trial,
+                                num_questions_per_player=1)
 
         # Navigate to take survey
         self.nav_to_word_surveys()
@@ -91,19 +121,6 @@ class WordsTest(FunctionalTest):
         given = self.browser.find_element_by_id('id_word').text
         self.assertEquals(given, catch_trial)
 
-
-    def nav_to_word_surveys(self):
-        self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_words_list').click()
-
-    def fill_form(self, element_id, text):
-        self.browser.find_element_by_id(element_id).send_keys(text)
-
-    def submit_answer(self):
-        choices_css = "[type='radio']"
-        choices = self.browser.find_elements_by_css_selector(choices_css)
-        choices[1].click()
-        self.browser.find_element_by_id('submit-id-submit').click()
 
 def stringify(items):
     return ','.join(map(str, items))
